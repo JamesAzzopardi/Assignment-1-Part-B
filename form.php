814bf1f4fc33cfbf94ecc8e157fd60585dcf2a5a
s3286310 <s3286310@student.rmit.edu.au>
2012-08-05 15:20:19 +1000

    Basic Functionality

---
+++ form.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html401/loose.dtd">
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<title>Wine Select Form</title>
+	</head>
+
+	<body>
+
+  		<form action="query.php" method="GET">
+			<table>
+				<tr><td><input type="checkbox" name="option" value="option1" /></td><td>Wine Name:</td><td><input type="text" name="wineName" /></td></tr>
+				<tr><td><input type="checkbox" name="option" value="option2" /></td><td>Winery Name:</td><td><input type="text" name="wineryname" /></td></tr>	
+				<tr><input type="submit" value="Show wines"></tr>			
+			</table>
+		</form>
+
+	</body>
+</html>	
+
+
