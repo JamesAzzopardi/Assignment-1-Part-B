75c1c66ded42de580dea7e0c705539f2f30e8ede
s3286310 <s3286310@student.rmit.edu.au>
2012-08-05 20:18:07 +1000

    Functionality Added

--- query.php
+++ query.php
@@ -6,29 +6,62 @@
 	</head>
 
 	<body>
+		<?php
+			  function showerror() {
+				 die("Error " . mysql_errno() . " : " . mysql_error());
+			  }
+			//Gets the information required to connect from the db.php
+			require 'db.php';
+			
+			// get the user data
+			$wineName = $_GET['wineName'];
+			
+			// Connect to the MySQL server
+			if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
+				die("Could not connect");
+			}	
+			mysql_select_db("winestore", $connection);
+			$query = "SELECT wine_name, winery_name
+				FROM wine, winery
+				WHERE wine.winery_id = winery.winery_id";
+			
+			
+			if (isset($wineName) && $wineName != "") {
+			$query .= " AND wine_name = '{$wineName}' ";
+			} 
+			
+			function displayWines($connection, $query) {
+			
+			// Run the query on the server
+			if (!($result = @ mysql_query ($query, $connection))) {
+			  showerror();
+			}
+			
+			$rowsFound = @ mysql_num_rows($result);
 
-	<?php
-		print $winename;
-  		require 'db.php';
-	       $connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
-  		mysql_select_db("winestore", $connection);
-		$query = "SELECT * FROM wine where wine_name = '$wineName'";
-		$result = mysql_query($query, $connection);
+			// If the query has results ...
+			if ($rowsFound > 0) {
+			  // and start a <table>.
+			  print "\n<table>\n<tr>" .
+				  "\n\t<th>Wine Name</th>" .
+				  "\n\t<th>Winery</th></tr>" ;
 
+			  // Fetch each of the query rows
+			  while ($row = @ mysql_fetch_array($result)) {
+				// Print one row of results
+				print "\n<tr>\n\t<td>{$row["wine_name"]}</td>" .
+					"\n\t<td>{$row["winery_name"]}</td>\n</tr>";
+			  } // end while loop body
 
-		 while ($row = mysql_fetch_row($result)) {
-  		 for ($i = 0; $i < mysql_num_fields($result); $i++) {
-    		  echo $row[$i] . " ";
-   		}
-   // Print a carriage return to neaten the output
-   echo "\n";
-  }
+			  // Finish the <table>
+			  print "\n</table>";
+			} // end if $rowsFound body
 
-  // Finish the HTML document
-  echo "</pre>";
-
-  // (4) Close the database connection
-  mysql_close($connection);
-	?>
+			// Report how many rows were found
+			print "{$rowsFound} records found matching your criteria<br>";
+		  } // end of function
+		  
+			displayWines($connection, $query);
+		?>
 	</body>
 </html>	
